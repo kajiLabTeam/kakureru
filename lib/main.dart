fa4ce05bdcea16506fd99192854f314609a065a0
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2026-07-31 17:30:03 +0900

    必要パッケージ追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
+/// This widget is the root of your application.
 class MyApp extends StatelessWidget {
+  /// Creates the root widget of the application.
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,18 +36,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// This widget is the home page of your application. It is stateful, meaning
+/// that it has a State object (defined below) that contains fields that affect
+/// how it looks.
+///
+/// This class is the configuration for the state. It holds the values (in this
+/// case the title) provided by the parent (in this case the App widget) and
+/// used by the build method of the State. Fields in a Widget subclass are
+/// always marked "final".
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  /// Creates the home page, displaying [title] in the app bar.
+  const MyHomePage({required this.title, super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  /// The text shown in the app bar.
   final String title;
 
   @override
